a39db2260ad441bccdf13fcd281048debd915528
Pranav M <50600962+p-m-ishra@users.noreply.github.com>
2019-07-23 15:04:03 -0400

    analyzing differences of pressures using function

--- PeakFinder.m
+++ PeakFinder.m
@@ -1,8 +1,16 @@
-REQUIREMENTFORLOCALMAX = 0.0004; #how high must a peak be to be a local max
-USERESTIMATEFORMAXIMA = 21; #how many maxima (software check)
-INTERVALCHECKRIGHT = 20; #how far right of the maxima should the algorithm check for minima
-INTERVALCHECKLEFT = 0; #how far left to check(usually 0)
-HOWFARAPART = 65;
+#Use Figure 1 for P0 and Figure 2 for P1
+REQUIREMENTFORLOCALMAXP0 = 0.0004; #how high must a peak be to be a local max
+USERESTIMATEFORMAXIMAP0 = 20; #how many maxima (software check)
+INTERVALCHECKRIGHTP0 = 15; #how far right of the maxima should the algorithm check for minima
+INTERVALCHECKLEFTP0 = 0; #how far left to check(usually 0)
+HOWFARAPARTP0 = 65;
+
+REQUIREMENTFORLOCALMAXP1 = 0.00027; #how high must a peak be to be a local max
+USERESTIMATEFORMAXIMAP1 = 20; #how many maxima (software check)
+INTERVALCHECKRIGHTP1 = 15; #how far right of the maxima should the algorithm check for minima
+INTERVALCHECKLEFTP1 = 0; #how far left to check(usually 0)
+HOWFARAPARTP1 = 65;
+
 FILENAMEP0 = "c1278P0.csv";
 FILENAMEP1 = "c1278P1.csv";
 FILENAMEOFFP0 = "c1278P0off.csv";
@@ -33,103 +41,121 @@ for i = 1:length(dataP1)
     dataP1(i) = dataP1(i) - dataOffFilteredAvgP1;
 end
 
-[peakVal, peakLoc] = findpeaks(dataP0,"DoubleSided", "MinPeakDistance", 3);
-LocalMaxInterval = [];
-localmaxintervalcount = 1;
-for i = 1:length(peakLoc)
-  if(peakVal(i) > REQUIREMENTFORLOCALMAX)
-    LocalMaxInterval(localmaxintervalcount) = peakLoc(i);
-    localmaxintervalcount = localmaxintervalcount + 1;
-  end
-end
-interval = [];
-index = 1;
-LocalMinima = [];
-LocalMaxima = [];
-LocalMaximaPeakLoc = [];
-LocalMinimaPeakLoc = [];
-localmaximacount = 1;
-localminimacount =1;
-iterator = 1;
-lowerbound = 0;
-upperbound = 0;
-while(!(upperbound>length(dataP0)) && !(iterator>length(LocalMaxInterval)))
-  lowerbound = LocalMaxInterval(iterator)-INTERVALCHECKLEFT;
-  upperbound = LocalMaxInterval(iterator) +INTERVALCHECKRIGHT;
-  if(upperbound >length(dataP0))
-    upperbound = length(dataP0);
-  end
-  fprintf("lowerbound: %d\t upperbound: %d\n", lowerbound, upperbound);
-  for i = lowerbound:upperbound
-      interval(index) = dataP0(i);
-      index = index +1;
-  end
-  TempMinimum = min(interval);
-  TempMaximum = max(interval);
-  for i = lowerbound: upperbound
-    if(dataP0(i) == TempMinimum)
-      if(localminimacount > 1)
-        if(abs(i-LocalMinimaPeakLoc(localminimacount-1)) > HOWFARAPART)
-          LocalMinima(localminimacount) = min(interval);
-          LocalMinimaPeakLoc(localminimacount) = i;
-          localminimacount = localminimacount + 1;
+    function[Minima,Maxima] = ExtremaFinder(dataa, reqlocalmax, userestimatemaxima,intervalcheckright, intervalcheckleft, howfarapart)
+      [peakVal, peakLoc] = findpeaks(dataa,"DoubleSided", "MinPeakDistance", 3);
+      LocalMaxInterval = [];
+      localmaxintervalcount = 1;
+      for i = 1:length(peakLoc)
+        if(peakVal(i) > reqlocalmax)
+          LocalMaxInterval(localmaxintervalcount) = peakLoc(i);
+          localmaxintervalcount = localmaxintervalcount + 1;
         end
-      else
-        LocalMinima(localminimacount) = min(interval);
-        LocalMinimaPeakLoc(localminimacount) = i;
-        localminimacount = localminimacount +1;
-      end
-    elseif(dataP0(i) == TempMaximum)
-      if(localmaximacount > 1)
-        if(abs(i - LocalMaximaPeakLoc(localmaximacount-1)) > HOWFARAPART)
-          LocalMaxima(localmaximacount) = max(interval);
-          LocalMaximaPeakLoc(localmaximacount) = i;
-          localmaximacount = localmaximacount + 1;
+      end   
+      interval = [];
+      index = 1;
+      LocalMinima = [];
+      LocalMaxima = [];
+      LocalMaximaPeakLoc = [];
+      LocalMinimaPeakLoc = [];
+      localmaximacount = 1;
+      localminimacount =1;
+      iterator = 1;
+      lowerbound = 0;
+      upperbound = 0;  
+      while(!(upperbound>length(dataa)) && !(iterator>length(LocalMaxInterval)))
+        lowerbound = LocalMaxInterval(iterator)-intervalcheckleft;
+        upperbound = LocalMaxInterval(iterator) +intervalcheckright;
+        if(upperbound >length(dataa))
+          upperbound = length(dataa);
         end
-      else
-        LocalMaxima(localmaximacount) = max(interval);
-        LocalMaximaPeakLoc(localmaximacount) = i;
-        localmaximacount = localmaximacount + 1;
+        for i = lowerbound:upperbound
+            interval(index) = dataa(i);
+            index = index +1;
+        end
+        TempMinimum = min(interval);
+        TempMaximum = max(interval);
+        for i = lowerbound: upperbound
+          if(dataa(i) == TempMinimum)
+            if(localminimacount > 1)
+              if(abs(i-LocalMinimaPeakLoc(localminimacount-1)) > howfarapart)
+                LocalMinima(localminimacount) = min(interval);
+                LocalMinimaPeakLoc(localminimacount) = i;
+                localminimacount = localminimacount + 1;
+              end
+            else
+              LocalMinima(localminimacount) = min(interval);
+              LocalMinimaPeakLoc(localminimacount) = i;
+              localminimacount = localminimacount +1;
+            end
+          elseif(dataa(i) == TempMaximum)
+            if(localmaximacount > 1)
+              if(abs(i - LocalMaximaPeakLoc(localmaximacount-1)) > howfarapart)
+                LocalMaxima(localmaximacount) = max(interval);
+                LocalMaximaPeakLoc(localmaximacount) = i;
+                localmaximacount = localmaximacount + 1;
+              end
+            else
+              LocalMaxima(localmaximacount) = max(interval);
+              LocalMaximaPeakLoc(localmaximacount) = i;
+              localmaximacount = localmaximacount + 1;
+            end
+          end
+        end
+
+        index = 1;
+        interval = [];
+        iterator = iterator +1;
+      endwhile
+      Minima = LocalMinima;
+      Maxima = LocalMaxima;
+      plot(dataa);
+      hold on
+      for i = 1:length(LocalMinima)
+        scatter(LocalMinimaPeakLoc(i), LocalMinima(i), 280);
       end
+      for i =1:length(LocalMaxima)
+        scatter(LocalMaximaPeakLoc(i), LocalMaxima(i), 280);
+      end
+      hold off
     end
-  end
+figure(1);
+[MinimaP0, MaximaP0] = ExtremaFinder(dataP0, REQUIREMENTFORLOCALMAXP0, USERESTIMATEFORMAXIMAP0, INTERVALCHECKRIGHTP0, INTERVALCHECKLEFTP0,HOWFARAPARTP0);
+figure(2);
+[MinimaP1, MaximaP1] = ExtremaFinder(dataP1, REQUIREMENTFORLOCALMAXP1, USERESTIMATEFORMAXIMAP1, INTERVALCHECKRIGHTP1, INTERVALCHECKLEFTP1, HOWFARAPARTP1);
 
-  index = 1;
-  interval = [];
-  iterator = iterator +1;
-endwhile
-disp("RESULTS\n\n");
-for i = 1: length(LocalMaxima)
-  fprintf("Local Maxima Number: %i\t\tLocal Maxima Value: %d\t\t Local Maxima Pos: %d\n", i, LocalMaxima(i), LocalMaximaPeakLoc(i));
-end  
-fprintf("Local Maxima Average: %d\n", mean(LocalMaxima));
-fprintf("Local Maxima Standard Deviation: %d\n\n", std(LocalMaxima));
+FinalMinima = [];
+FinalMaxima = [];
+for i = 1:length(MinimaP0)
+  FinalMinima(i) = abs(MinimaP1(i) - MinimaP0(i));
+  FinalMaxima(i) = abs(MaximaP1(i) - MaximaP0(i));
+endfor
+FinalMinimaAvg = mean(FinalMinima);
+FinalMaximaAvg = mean(FinalMaxima);
+FinalMinimaStd = std(FinalMinima);
+FinalMaximaStd = std(FinalMaxima);
+disp("RESULTS\n");
+disp("LOCAL MINIMA, avg, and stdev <-- in that order\n");
+fprintf('%d\n', FinalMinima);
+fprintf('%s\n%d\n', "Average", FinalMinimaAvg);
+fprintf('%s\n%d\n', "Standard Deviation", FinalMinimaStd);
+disp("LOCAL MAXIMA, avg, and stdev <-- in that order\n");
+fprintf('%d\n', FinalMaxima);
+fprintf('%s\n%d\n', "Average", FinalMaximaAvg);
+fprintf('%s\n%d\n', "Standard Deviation", FinalMaximaStd);
 
-for i = 1: length(LocalMinima)
-  fprintf("Local Minima Number: %i\t\tLocal Minima Value: %d\t\t Local Minima Pos: %d\n", i, LocalMinima(i), LocalMinimaPeakLoc(i));
-end  
-fprintf("Local Minima Average: %d\n", mean(LocalMinima));
-fprintf("Local Minima Standard Deviation: %d\n\n", std(LocalMinima));
-if(USERESTIMATEFORMAXIMA == length(LocalMaxima))
-  fprintf("Software estimate was CORRECT according to the user estimate\n")
-else
-  fprintf("Software estimate was INCORRECT according to the user estimate, please manually check the data.\n")
-end
-plot(dataP0);
-hold on
-for i = 1:length(LocalMinima)
-  scatter(LocalMinimaPeakLoc(i), LocalMinima(i), 280);
-end
-for i =1:length(LocalMaxima)
-  scatter(LocalMaximaPeakLoc(i), LocalMaxima(i), 280);
-end
-hold off  
-LocalMinimaStats = [mean(LocalMinima) std(LocalMinima)];
-LocalMaximaStats = [mean(LocalMaxima) std(LocalMaxima)];
-
+dlmwrite("Results.csv", "LocalMinima");
+dlmwrite("Results.csv", FinalMinima, '-append');
+dlmwrite("Results.csv", FinalMinimaAvg, '-append');
+dlmwrite("Results.csv", FinalMinimaStd, '-append');
+dlmwrite("Results.csv", "LocalMaxima", '-appened');
+dlmwrite("Results.csv", FinalMaxima, '-append');
+dlmwrite("Results.csv", FinalMaximaAvg, '-append');
+dlmwrite("Results.csv", FinalMaximaStd, '-append');
+%{
 dlmwrite("Results.csv", "LocalMinimaList, Avg,and Standard Deviation <-- in that order");
 dlmwrite("Results.csv", LocalMinima, '-append');
 dlmwrite("Results.csv", LocalMinimaStats, '-append');
 dlmwrite("Results.csv", "LocalMaximaList, Avg,and Standard Deviation <-- in that order", '-append');
 dlmwrite("Results.csv", LocalMaxima, '-append');
 dlmwrite("Results.csv", LocalMaximaStats, '-append');
+%}
